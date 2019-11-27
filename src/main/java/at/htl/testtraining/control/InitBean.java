a185787b0d23bd4c64da4dfcbc8169217899e27e
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-27 20:37:08 +0100

    Implemented the methods readTeamsAndDriversFromFile and persistTeamsAndDrivers

--- src/main/java/at/htl/testtraining/control/InitBean.java
+++ src/main/java/at/htl/testtraining/control/InitBean.java
@@ -1,6 +1,8 @@
 package at.htl.testtraining.control;
 
+import at.htl.testtraining.entity.Driver;
 import at.htl.testtraining.entity.Race;
+import at.htl.testtraining.entity.Team;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
@@ -44,23 +46,39 @@ public class InitBean {
     }
 
     void persistRaces(String[] line) {
+        Race race = null;
         try {
-            Race race = null;
             race = em.createNamedQuery("Race.findRaceById", Race.class)
                     .setParameter("ID", Long.valueOf(line[0]))
                     .getSingleResult();
         } catch (NoResultException e) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            Race raceP = new Race(line[1], LocalDate.parse(line[2], formatter));
-            em.persist(raceP);
+            race = new Race(line[1], LocalDate.parse(line[2], formatter));
+            em.persist(race);
         }
     }
 
     private void readTeamsAndDriversFromFile(String file) {
-
+        URL url = Thread.currentThread().getContextClassLoader().getResource(TEAMS_DRIVERS_FILE);
+        try (Stream<String> stream = Files.lines(Paths.get(url.getPath()), StandardCharsets.UTF_8)) {
+            stream.skip(1).map(s -> s.split(";"))
+                    .forEach(this::persistTeamsAndDrivers);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     void persistTeamsAndDrivers(String[] line) {
-
+        Team team = null;
+        try {
+            team = em.createNamedQuery("Team.findTeamByName", Team.class)
+                    .setParameter("NAME", line[0])
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            team = new Team(line[0]);
+            em.persist(team);
+        }
+        em.persist(new Driver(line[1], team));
+        em.persist(new Driver(line[2], team));
     }
 }
