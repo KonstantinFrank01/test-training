9a05d1d1c5fc07f47c77a0c20cc5709020c45652
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-27 19:54:21 +0100

    Implemented Class Race

--- src/main/java/at/htl/testtraining/entity/Race.java
+++ src/main/java/at/htl/testtraining/entity/Race.java
@@ -1,4 +1,51 @@
 package at.htl.testtraining.entity;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Table(name = "F1_RACE")
+@Entity
 public class Race {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String country;
+    private LocalDate date;
+
+    public Race() {
+    }
+
+    public Race(String country, LocalDate date) {
+        this.country = country;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /*public void setId(Long id) {
+        this.id = id;
+    }*/
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s %s", id, country, date);
+    }
 }
