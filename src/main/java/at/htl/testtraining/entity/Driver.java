d8097d75d6ad59e206eb91ceba91b090ad7b8c38
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-27 19:46:01 +0100

    Implemented Class Driver

--- src/main/java/at/htl/testtraining/entity/Driver.java
+++ src/main/java/at/htl/testtraining/entity/Driver.java
@@ -1,4 +1,52 @@
 package at.htl.testtraining.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "F1_DRIVER")
 public class Driver {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private Team team;
+
+    public Driver() {
+    }
+
+    public Driver(String name, Team team) {
+        this.name = name;
+        this.team = team;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /*public void setId(Long id) {
+        this.id = id;
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s %s", id, name, team);
+    }
 }
