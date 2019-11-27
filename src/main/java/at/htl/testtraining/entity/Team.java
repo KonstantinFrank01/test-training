a8dc5936ea4710d055fb957c84ca69739f01c9a4
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-11-27 19:48:14 +0100

    Implemented Class Team

--- src/main/java/at/htl/testtraining/entity/Team.java
+++ src/main/java/at/htl/testtraining/entity/Team.java
@@ -1,4 +1,40 @@
 package at.htl.testtraining.entity;
 
+import javax.persistence.*;
+
+@Table(name = "F1_TEAM")
+@Entity
 public class Team {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Team() {
+    }
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /*public void setId(Long id) {
+        this.id = id;
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s", id, name);
+    }
 }
